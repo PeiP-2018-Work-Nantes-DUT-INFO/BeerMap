57479e0ed38e41300e2e194b43e6ca0f9522e6ec
Nathan ROCHER <nathan.rocher@etu.univ-nantes.fr>
2020-03-05 10:48:45 +0100

    Up searchbar

--- static/src/App.js
+++ static/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import SearchBar from './Component/SearchBar/SearchBar';
+
+import ReactMapboxGl, {Layer, Feature} from 'react-mapbox-gl';
+
+const Map = ReactMapboxGl({
+  accessToken: 'pk.eyJ1IjoidGhlbmF0aGFuMzAiLCJhIjoiY2ozazU2MGtvMDAyYTJ3anZ1N21zY2preCJ9.FAwz_TGb' +
+      'KlqROzSbx7qbww'
+});
+
+export default class App extends React.Component {
+
+  render() {
+    return (
+      <div className="AppBlock">
+        <SearchBar/>
+        <Map
+          style="mapbox://styles/mapbox/streets-v9"
+          containerStyle={{
+          display: "block",
+          width: "100%",
+          height: "100%"
+        }}>
+          <Layer
+            type="symbol"
+            id="marker"
+            layout={{
+            'icon-image': 'marker-15'
+          }}>
+            <Feature coordinates={[-0.481747846041145, 51.3233379650232]}/>
+          </Layer>
+        </Map>
+      </div>
+    )
+  }
 }
-
-export default App;
---
+++ static/src/Component/SearchBar/SearchBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import "./SearchBar.css"
+
+export default class SearchBar extends React.Component {
+
+  render() {
+    return (
+      <div id="SearchBar">
+        
+        <input type="text" />
+
+      </div>
+    )
+  }
+}
