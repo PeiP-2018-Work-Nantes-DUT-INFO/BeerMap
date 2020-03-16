4b028d1a5062f6540655ac63497f0ec38b9046f8
Nathan R <3007nat@gmail.com>
2020-03-16 08:34:23 +0100

    Changement d'emplacement du dossier static
    Co-authored-by: Josik SB <josiksb@hotmail.com>
    Co-authored-by: maxence.leguede@outlook.fr <maxence.leguede@outlook.fr>

--- projet/index.js
+++ projet/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
+
+///////////////////////////////////////////////
+const http = require('http').createServer(app);
+///////////////////////////////////////////////
+
 const bodyParser = require("body-parser");
+const io = require('socket.io')(http);
 const database = require('./app/config/dbconfig');
 const port = process.argv[2] || 3000;
 
@@ -12,18 +18,30 @@ database
     .init
     .then((db) => {
 
-        app.listen(port, function () {
-            console.log("Server listening on port : " + port);
-        });
-
         app.use(bodyParser.urlencoded({extended: false}));
         app.use(bodyParser.json());
 
         /* Router configuration */
         app.use('/api', require('./app/routes/router'));
 
+        io.on('connection', socket => {
+            console.log('a user connected');
+            socket.on('search', function(msg){
+
+                console.log("Recherche : ", msg)
+
+
+
+                io.emit('search-response', msg);
+            });
+        });
+
         //accès aux pages statiques
-        app.use(express.static('../static/build/'));
+        app.use(express.static('./static/build/'));
+
+        http.listen(port, function () {
+            console.log("Server listening on port : " + port);
+        });
 
     })
     .catch(err => {console.log(err)});
